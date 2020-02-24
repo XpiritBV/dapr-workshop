d73ec255d0ec9095e8ecee68452d8c5e6497a840
Geert van der Cruijsen <geert@vdcruijsen.net>
2020-02-24 22:54:39 +0100

    k8s deployment

---
+++ OrderApp/OrderApi/Controllers/PingController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OrderApi.Controllers
+{
+    [Route("/[controller]")]
+    [ApiController]
+    public class PingController : ControllerBase
+    {
+        // GET: api/Ping
+        [HttpGet]
+        public string Get()
+        {
+            return "Everything OK!";
+        }
+
+
+    }
+}
